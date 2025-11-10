9b132670d734328561fc836eacc5d49a1ef87aa2
cschneiderx <christianespacoalameda@gmail.com>
2025-11-10 17:00:23 -0300

    criando jupyter para não ter que ficar lendo o código toda hora

--- teste.py
+++ teste.py
@@ -2,9 +2,17 @@ import pandas as pd
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-dataset = pd.read_csv('hotel_bookings.csv', sep=',', index_col=0)
+dataset = pd.read_csv('hotel_bookings.csv', sep=',', index_col= None)
 
 #emJulho = dataset[dataset['arrival_date_month'] =='July']
 #print(emJulho)
-dataset.isnull()
-print(dataset.head(1))
+#if dataset.isna() == True:
+ #   print("a")
+
+print(dataset.isna().sum())
+##Company
+#testeAgent = dataset['agent']
+#print(testeAgent)
+
+#testeCompany = dataset['company']
+#print(testeCompany)
